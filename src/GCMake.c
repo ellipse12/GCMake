046586c50d38d62b84311068de19ea0f93082725
ellipse12 <ellipse12@debian.com>
2024-01-19 12:24:51 -0700

    Started on file walk implementation

--- src/GCMake.c
+++ src/GCMake.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 500
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <ftw.h>
@@ -20,7 +22,7 @@ typedef struct {
 
 const char* gcmake_read(){
     FILE* fp;
-    if(fopen_s(&fp, "GCMake.json", "r+") != 0){
+    if((fp = fopen("GCMake.json", "r+")) == NULL){
         perror("Could not find GCMake.json file.");
         exit(1);
     }
@@ -41,7 +43,7 @@ const char* gcmake_read(){
 
 }
 
-size_t iterate_json(cJSON* jsonObject,const char* str, char** out){
+size_t iterate_json(cJSON* jsonObject,const char* str, const char** out){
     cJSON* jsonArray = cJSON_GetObjectItemCaseSensitive(jsonObject, str);
     if(cJSON_IsArray(jsonArray)){
         size_t arrsize = cJSON_GetArraySize(jsonArray);
@@ -69,13 +71,32 @@ void gcmake_parse(Data* out, const char* data){
     out->json = json;
     out->file_patterns_length = iterate_json(json, "files", NULL);
     if(out->file_patterns_length == -1){
-        printf("Could not find \"files\" parameter in GCMake.json, cannot continue...");
+        printf("Could not find required parameter: \"files\", in GCMake.json.");
         exit(1);
         
     }
     out->file_patterns = malloc(sizeof(char) * out->file_patterns_length);
     iterate_json(json, "files", out->file_patterns);
+    
+    out->dirs_patterns_length = iterate_json(json, "dirs", NULL);
+    if(out->dirs_patterns_length == -1){
+        printf("Could not find required parameter: \"dirs\", in GCMake.json.");
+        exit(1);
+    }
+    out->dirs_patterns = malloc(sizeof(char) * out->dirs_patterns_length);
+    iterate_json(json, "dirs", out->dirs_patterns);
+}
 
+int gcmake_visit(const char* path, const struct stat* stats, int type, struct FTW* ftwbuf){
+	printf("%s\n", path);
+	return type==FTW_D ? FTW_SKIP_SUBTREE : FTW_CONTINUE;
+}
+
+void gcmake_run(Data* data){
+	if(nftw(".", gcmake_visit, 10, FTW_ACTIONRETVAL) == -1){
+		perror("nftw");
+		exit(EXIT_FAILURE);
+	}
 }
 
 
@@ -85,6 +106,9 @@ int main(){
     
     Data da = {0};
     gcmake_parse(&da, data);
-     
+    for(int i = 0; i < da.file_patterns_length; i++){
+        printf("pattern: %s\n", da.file_patterns[i]);
+    }     
+	gcmake_run(&da);
     return 0;
 }
